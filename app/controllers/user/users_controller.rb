b930de5f2d8f385f736472d4d372e61576d93a6f
nokia0315 <naoki11923611@gmail.com>
2021-01-11 14:31:52 +0000

    [Update]会員マイページ作成

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,7 +1,7 @@
 class User::UsersController < ApplicationController
   def show
-    @customer = current_customer
-    # @customer = Customer.find(params[:id])
+    @user = current_user
+    # @user = user.find(params[:id])
   end
 
   def quit
@@ -9,27 +9,27 @@ class User::UsersController < ApplicationController
 
 
   def out
-    @customer = current_customer
-    @customer.update(user_status: true)
+    @user = current_user
+    @user.update(is_valid: true)
     reset_session
     flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
     redirect_to root_path
   end
 
   def edit
-    @customer = current_customer
-    # @customer = Customer.find(params[:id])
+    @user = current_user
+    # @user = user.find(params[:id])
   end
 
   def update
-    @customer = Customer.find(params[:id])
-    @customer.update(customer_params)
-    redirect_to customer_customer_path(@customer.id)
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
 
-  def customer_params
+  def user_params
     params.require(:user).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :residence, :phone_number, :email, :is_valid)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   devise_for :farmers
   devise_for :users
   root to: 'job_offers#top'
+  scope module: :user do
+    resources :users do
+    		collection do
+    	     get 'quit'
+    	     patch 'out'
+    	  end
+    end
+  end
 end
