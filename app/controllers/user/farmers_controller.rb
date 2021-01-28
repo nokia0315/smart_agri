217eacf9048ba19f5c5c6df6212d2b89b73c4f10
nokia0315 <naoki11923611@gmail.com>
2021-01-28 14:46:07 +0000

    [Update]farmerの詳細ページレイアウト修正

--- app/controllers/user/farmers_controller.rb
+++ app/controllers/user/farmers_controller.rb
@@ -5,8 +5,8 @@ class User::FarmersController < ApplicationController
 
   def show
     @farmer = Farmer.find(params[:id])
-    @reviews = @farmer.reviews.page(params[:page]).per(5).order(id: "DESC")
-    @blogs = @farmer.blogs.page(params[:page]).per(5).order(id: "DESC") ## idの降順
+    @reviews = @farmer.reviews.page(params[:page]).per(3).order(id: "DESC")
+    @blogs = @farmer.blogs.page(params[:page]).per(3).order(id: "DESC") ## idの降順
     @job_offers = @farmer.job_offers.page(params[:page]).per(5).order(id: "DESC") ## idの降順
   end
 
