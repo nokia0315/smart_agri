6486b454a9e4996954e8b549f82c8806fe1fa2bd
nokia0315 <naoki11923611@gmail.com>
2021-02-23 09:11:52 +0000

    [Updata]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
+  ## Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
